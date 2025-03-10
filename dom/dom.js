1919efefb5ba8c2f9b6a375aee1b079da7d2e4d7
Megha <jangidmegha2002@gmail.com>
2025-03-10 23:11:53 +0530

    with new dom files

---
+++ dom/dom.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const randomnum=Math.trunc(Math.random()*6+1);
+console.log(randomnum);
+
+// create a image var
+const image1 =  "/dom/dice/d" + randomnum + ".jpg";
+//  this above expression is written with reference to images like d1.jpg or d2.jpg etc
+// so for 'd', there will be the path of image; for nums like 1/2or 3 etc we added randomnum
+// that's why we created randomnumber; and then last '.jpg'
+
+document.querySelector('img').setAttribute("src",image1);
+
+const play1= Math.trunc(Math.random()*6+1);
+const play2= Math.trunc(Math.random()*6+1);
+console.log(play1,play2);
+
+if(play1>play2)
+    document.querySelector('#result').innerHTML='player1 wins';
+else if(play1<play2)
+document.querySelector('#result').innerHTML='player2 wins';
+
+else
+document.querySelector('#result').innerHTML='DRAW';
