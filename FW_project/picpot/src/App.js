36682e3d67ea17cd4272f248abbde5817659ec30
Megha <jangidmegha2002@gmail.com>
2025-03-18 23:55:27 +0530

    react code sample html css js

--- FW_project/picpot/src/App.js
+++ FW_project/picpot/src/App.js
@@ -1,5 +1,28 @@
 import React from "react";
- 
-const app = () => {} ;
+
+import "./App.css";
+
+const app = () => {
+
+    // javascript
+    const otp = Math.trunc(Math.random()*10000 +1);
+
+    return(
+        // only one element in return state. so <p></p> & <li></li> in one <div></div>
+        <div>
+            <p>hello REACT</p>
+            <p>the otp is {otp}</p>
+            <ul>
+            <li>one</li>
+            <li>two</li>
+            <li>three</li>
+            </ul>
+        </div>
+        
+
+    )
+
+
+} ;
 
 export default app;
