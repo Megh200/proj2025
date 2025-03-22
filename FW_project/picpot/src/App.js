49afe835d9e18b665b6100dff2d742aff449f0b8
Megha <jangidmegha2002@gmail.com>
2025-03-22 23:12:22 +0530

    Hook function {useState()}

--- FW_project/picpot/src/App.js
+++ FW_project/picpot/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {useState} from "react";
 
 import AddItem from "./components/additem.js";
 import Todolist from "./components/todolist.js";
@@ -12,13 +12,26 @@ const App = () => {
 
     // to show this below data in todolist.js
     // taking objects in an array
-    const array = [
-                        {id:1, work:"no work", td:'today', tm:'tommorow'},
-                        {id:2, work:"reading", td:'today', tm:'tommorow'},
-                        {id:3, work:"working IT", td:'today', tm:'tommorow'},
-    ]
+
+    // const array = [
+    //                     {id:1, work:"no work", td:'today', tm:'tommorow'},
+    //                     {id:2, work:"reading", td:'today', tm:'tommorow'},
+    //                     {id:3, work:"working IT", td:'today', tm:'tommorow'},
+    // ]
+
+
+    // taking this const array in hook function:- useState() by [variable,function]
+    const [array, funofhook] = useState(
+                             [  {id:1, work:"no work", td:'today', tm:'tommorow'},
+                                {id:2, work:"reading", td:'today', tm:'tommorow'},
+                                {id:3, work:"working IT", td:'today', tm:'tommorow'},   ]                
+    )
+
 
     const additemhandle =(newitem) =>{
+        
+        // adding useState function in this bcz we have to show the array so here it will push in array
+
         array.push(newitem);
         console.log(array);
     }
